fa783b3aceb783ad309d2373140c010b692285ba
Amrinder Singh <80534489+Amrinder-S@users.noreply.github.com>
2022-08-25 12:42:11 +0530

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -8,6 +8,6 @@ for %%x in (%*) do (
 )
 ::
 echo [92mCompile.bat by "github.com/Amrinder-S" [0m
-for /L %%i in (1,1,%argCount%) do echo [91mCompiling[0m [92m!argVec[%%i]! [0m [91m to [0m [92m!argVec[%%i]:~0,-4!.exe [0m  && c++ !argVec[%%i]! -o !argVec[%%i]:~0,-4!.exe
+for /L %%i in (1,1,%argCount%) do echo [91mCompiling[0m [92m!argVec[%%i]! [0m [91m to [0m [92m!argVec[%%i]:~0,-4!.exe [0m  && c++ !argVec[%%i]! -o !argVec[%%i]:~0,-4!.exe > nul
 :: To add selection (y/n) if we want to execute the compiled programs or not.
 :: Also to add an installer.bat to install this batch script in path
